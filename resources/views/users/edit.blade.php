41a74eaae727054574adf26b8e51a4154234398f
toxicIam <104590833+toxicIam@users.noreply.github.com>
2023-07-26 15:00:07 +0600

    Update edit.blade.php

--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -1,24 +1,24 @@
 @extends('layouts.app')
 
 @section('body')
-    <h1 class="mb-0">Edit Users</h1>
+    <h1 class="mb-0">Редактировать пользователя</h1>
     <hr />
     <form action="{{ route('users.update', $users->id) }}" method="POST">
         @csrf
         @method('PUT')
         <div class="row">
             <div class="col mb-3">
-                <label class="form-label">Title</label>
+                <label class="form-label">ФИО</label>
                 <input type="text" name="name" class="form-control" placeholder="Name" value="{{ $users->name }}" >
             </div>
             <div class="col mb-3">
-                <label class="form-label">Price</label>
+                <label class="form-label">Email</label>
                 <input type="text" name="email" class="form-control" placeholder="Email" value="{{ $users->email }}" >
             </div>
         </div>
         <div class="row">
             <div class="col mb-3">
-                <label class="form-label">Product Code</label>
+                <label class="form-label">Возраст</label>
                 <input type="text" name="age" class="form-control" placeholder="Age" value="{{ $users->age }}" >
             </div>
         </div>
